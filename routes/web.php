a754777f96d54d431ddc524f8992a1c2b2eee0f8
akihiro-f <akihirobluesharks@gmail.com>
2021-11-21 15:01:18 +0900

    part9 end

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//ブログ一覧画面の表示
 Route::get('/', 'BlogController@showList')->name('blogs');
+//ブログ詳細画面の表示
+Route::get('/blog/{id}', 'BlogController@showDetail')->name('show');
